7f1cdfb3510b46c040afd6f52a4b64d377ef583c
arjunvenkatesh <arjun.venn@gmail.com>
2015-03-21 22:56:46 -0700

    Added initial functionality for multiple ships

--- battleship.py
+++ battleship.py
@@ -1,7 +1,12 @@
 from random import randint
 
-MAX_TURNS = 5
+MAX_TURNS = 10
+TOTAL_SHIPS = 5
+ROW = 0
+COLUMN = 1
+
 board = []
+ships = []
 
 # Our board will be a 5x5 grid of "O"s
 for x in range(5):
@@ -33,15 +38,32 @@ def random_row(board):
 def random_col(board):
     return randint(0, len(board[0]) - 1)
 
-# initialize the location of the ship
-ship_row = random_row(board)
-ship_col = random_col(board)
+# 
+# Param: ships, empty list of ships
+# Param: board 
+# Mutates: ships, adds a unique ship to the list of ships
+def createShip(ships, board):
+    shipRow = random_row(board)
+    shipCol = random_col(board)
+    ship = [shipRow, shipCol]
+
+    if ship not in ships:
+        ships.append(ship)
+    else:
+        createShip(ships, board)
+
+# initialize the random location of our ships
+for numberOfShips in range(TOTAL_SHIPS):
+    createShip(ships, board)
 
 # print our location for debugging puposes only
 #print(ship_row+1)
 #print(ship_col+1)
 
 gameOver = False
+
+#to keep track of how many ships we have sunk
+sunken = 0
 
 # Total of MAX_TURNS turns
 for turn in range(MAX_TURNS):
@@ -56,30 +78,43 @@ for turn in range(MAX_TURNS):
     printLine()
 
     #if the guess is right
-    if guess_row == ship_row and guess_col == ship_col:
-        board[ship_row][ship_col] = "^"
-        print_board(board)
-        print("Congratulations! You sunk my battleship!")
-        break
+    for ship in ships:
+        ship_row = ship[ROW]
+        ship_col = ship[COLUMN]
+
+        if guess_row == ship_row and guess_col == ship_col:
+            board[ship_row][ship_col] = "^"
+            print_board(board)
+            print("You sunk one of my battleships!")
+            sunken += 1
+
+            if( sunken == TOTAL_SHIPS):
+                printLine()
+                print("Congratulations you have sunk all my ships!")
+                print("YOU WIN! :)")
+                break
+
+            print("There are", TOTAL_SHIPS - sunken, "ships left!")
+
 
     #cases for when the guess is wrong
-    else:
-        if (guess_row < 0 or guess_row > 4) or \
-        (guess_col < 0 or guess_col > 4):
-            print("Oops, that's not even in the ocean.")
-
-        elif(board[guess_row][guess_col] == "X"):
-            print("You guessed that one already.")
-
         else:
-            print("You missed my battleship!")
-            board[guess_row][guess_col] = "X"
+            if (guess_row < 0 or guess_row > 4) or \
+            (guess_col < 0 or guess_col > 4):
+                print("Oops, that's not even in the ocean.")
 
-        if( turn == MAX_TURNS - 1 ):
-            print_board(board)
-            print("Out of turns, game Over :(")
-            printLine()
-            gameOver = True
+            elif(board[guess_row][guess_col] == "X"):
+                print("You guessed that one already.")
+
+            else:
+                print("You missed my battleship!")
+                board[guess_row][guess_col] = "X"
+
+            if( turn == MAX_TURNS - 1 ):
+                print_board(board)
+                print("Out of turns, game Over :(")
+                printLine()
+                gameOver = True
 
     #re-print for the next turn
     if( not gameOver ):
